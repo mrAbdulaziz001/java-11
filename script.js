e64cfa8754489cdfc52a843c4443e09ce7c7bb6a
mrAbdulaziz001 <klalwlelsl@gmail.com>
2025-04-19 10:12:13 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let box = document.querySelector(".box")
+
+let bgcolor = prompt("enter bgcolor")
+let color = prompt ("enter color")
+
+document.body.style.backgroundColor = bgcolor ;
+document.body.style.color = color;
